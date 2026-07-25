e2775d14848c1eedca04349d978f6358ddbae4d3
Wizard of Hahz <info@blkluv.org>
2026-07-25 17:38:13 -0400

    Update notification-bar.tsx

--- components/layout/notification-bar.tsx
+++ components/layout/notification-bar.tsx
@@ -6,9 +6,9 @@ import { useLocalStorage } from "@/lib/hooks/use-local-storage";
 
 const NOTIFICATION = {
 	version: "4",
-	message: "Polymaxx.bet is 2.6s faster than Polymarket's RTDS feed",
+	message: "Polymaxx.bet is 2.6s faster than Polymarket's RTDS feed. Create a new Polymarket account.",
 	cta: "Read the benchmark",
-	href: "https://www.struct.to/blog/polymarket-rtds-vs-struct-trades-ws",
+	href: "https://polymarket.com/?r=cremarket",
 } as const;
 
 
