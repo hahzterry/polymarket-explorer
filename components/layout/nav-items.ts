4b03dfbd4d9f3acd784f9ac2b988c3738b3fa5aa
Wizard of Hahz <info@blkluv.org>
2026-07-24 20:06:37 -0400

    Update nav-items.ts

--- components/layout/nav-items.ts
+++ components/layout/nav-items.ts
@@ -8,12 +8,11 @@ export type NavItem = {
 };
 
 export const NAV_ITEMS: NavItem[] = [
+	{ href: "https://polymarket.com/?r=cremarket" as Route, label: "Join Polymarket" },
 	{ href: "/events" as Route, label: "Events" },
 	{ href: "/markets" as Route, label: "Markets" },
 	{ href: "/traders" as Route, label: "Traders" },
 	{ href: "/builders" as Route, label: "Builders" },
 	{ href: "/tags" as Route, label: "Tags" },
 	{ href: "/analytics" as Route, label: "Analytics" },
-	{ href: "/rewards" as Route, label: "Rewards" },
-	{ href: "https://www.struct.to/rest-api" as Route, label: "API", external: true, primary: true },
 ];
